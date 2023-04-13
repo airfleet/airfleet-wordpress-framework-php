0dcededf4b618eed1e57abc8d00a6930c56f036d
Nuno Freitas <nunofreitas@gmail.com>
2023-04-13 14:30:57 +0100

    feat: plugin acf utils

---
+++ inc/Plugin/AcfAirfleetOptionsSubPage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Airfleet\Framework\Plugin;
+
+use Airfleet\Framework\Acf\AirfleetOptionsSubPage;
+
+/**
+ * Create an ACF options sub-page under the Airfleet menu.
+ */
+class AcfAirfleetOptionsSubPage extends AirfleetOptionsSubPage {
+	protected array $options;
+
+	/**
+	 * Constructor
+	 *
+	 * @param array $config The plugin config.
+	 * @param array $options Options sub-page properties.
+	 */
+	public function __construct( array $config, array $options = [] ) {
+		parent::__construct(
+			array_merge(
+				[
+					'page_title' => $config['title'],
+					'menu_title' => $config['short_title'],
+				] + $options
+			)
+		);
+	}
+}
