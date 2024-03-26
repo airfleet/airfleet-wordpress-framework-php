86bb7c0264d3a4da13d7cf01afbce0a4ce7b89fa
dbiljak <dbiljak@gmail.com>
2024-03-26 10:55:51 +0100

    Minor code refactor

--- inc/Options/Fields/MediaField.php
+++ inc/Options/Fields/MediaField.php
@@ -16,43 +16,62 @@ class MediaField extends Field {
 		wp_enqueue_script( 'media-field-script', $url_path, array(), null, true );
 	}
 
-	protected function attributes(array $args, mixed $value) : array {
+	protected function get_img_attributes( array $args, mixed $value ): array {
+		$media_types  = $args['media_types'] ?? [];
+		$media_types  = ! empty( $media_types ) ? implode( ",", $media_types ) : $media_types;
+		$min_width    = $args['min_width'] ?? 600;
+		$min_height   = $args['min_height'] ?? 400;
+		$instructions = $args['instructions'] ?? '';
+		$disabled     = $args['disabled'] ?? false;
+
 		return [
-			'type'         => $args['type'],
-			'name'         => $this->input_name(),
-			'id'           => $this->id,
-			'value'        => $value,
-			'title' 	   => $args['title'] ?? '',
-			'label' 	   => $args['label'] ?? '',
-			'required'     => $args['required'] ?? \false,
-			'disabled'     => $args['disabled'] ?? \false,
-			'min_width'    => $args['min_width'] ?? \false,
-			'max_width'    => $args['max_width'] ?? \false,
-			'min_height'   => $args['min_height'] ?? \false,
-			'max_height'   => $args['max_height'] ?? \false,
-			'instructions' => $args['instructions'] ?? '',
-			'media_types'  => $args['media_types'] ?? [],
+			'src'               => ! empty( $value ) ? $value : sprintf( 'https://placehold.co/%sx%s', $min_width, $min_height ),
+			'name'              => $this->input_name(),
+			'id'                => $this->id,
+			'class'             => ! $disabled ? 'js-image-upload' : '',
+			'width'             => $args['min_width'] ?? 100,
+			'alt'               => $args['label'] ?? '',
+			'title'             => $args['title'] ?? '',
+			'label'             => $args['label'] ?? '',
+			'data-min-width'    => $args['min_width'] ?? false,
+			'data-max-width'    => $args['max_width'] ?? false,
+			'data-min-height'   => $args['min_height'] ?? false,
+			'data-max-height'   => $args['max_height'] ?? false,
+			'data-instructions' => strip_tags( $instructions ),
+			'data-media-types'  => $media_types
+		];
+	}
+
+	protected function get_hidden_attributes( mixed $value ): array {
+		return [
+			'type'  => 'hidden',
+			'name'  => $this->input_name(),
+			'id'    => $this->id . '_url',
+			'value' => ! empty( $value ) ? $value : '',
 		];
 	}
 
 	protected function render_input( array $args, mixed $value ): void {
-		$label 			 = $args['label'] ?? '';
-		$url             = ! empty( $value ) ? $value : sprintf( 'https://placehold.co/%sx%s', $args['min_width'], $args['min_height'] );
-		$attrs           = $this->attributes( $args, $value );
-		$data_attributes = \sprintf( 'data-min-height="%s" data-min-width="%s" data-max-height="%s" data-max-width="%s" data-media-types="%s" data-instructions="%s"', $args['min_height'], $args['min_width'], $args['max_height'], $args['max_width'], \implode( ',', $args['media_types'] ), $args['instructions'] );
-		$img             = \sprintf( '<img class="js-image-upload" id="%s" style="width: %spx" alt="%s" src="%s" %s />', $this->id, $args['min_width'], $label, $url, $data_attributes );
-		$hidden          = \sprintf( '<input type="hidden" name="%s" id="%s_url" value="%s" />', $attrs['name'], $this->id, $url );
+		$disabled     = $args['disabled'] ?? false;
+		$img_attrs    = $this->get_img_attributes( $args, $value );
+		$hidden_attrs = $this->get_hidden_attributes( $value );
 
-		\printf( '%s%s', $img, $hidden );
+		$img    = sprintf( '<img %s />', Field::render_attributes( $img_attrs ) );
+		$hidden = '';
+
+		if ( ! $disabled ) {
+			$hidden = sprintf( '<input %s />', Field::render_attributes( $hidden_attrs ) );
+		}
+
+		printf( '%s%s', $img, $hidden );
 	}
 
 	protected function default_validate( mixed $value ): bool {
 		$required = isset( $this->args['required'] ) && $this->args['required'];
-
 		if ( $required && ! $value ) {
 			$this->add_error( "{$this->title} is required." );
 
-			return \false;
+			return false;
 		}
 
 		return parent::default_validate( $value );
